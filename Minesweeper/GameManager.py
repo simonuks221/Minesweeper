d132b956b6ebc46deb85cd59d037b2e99169b3c0
simonuks221 <simonasriauka@gmail.com>
2020-11-07 18:54:06 +0200

    Comments

--- Minesweeper/GameManager.py
+++ Minesweeper/GameManager.py
@@ -3,8 +3,9 @@ import numpy as np
 from MinesweeperTIle import Tile
 from tkinter import messagebox
 
-#Game manager script
+#Game manager script. ALl game logic here
 class GM:
+    #Game grid sizes
     gameSizeList = ["10x10", "15x15", "15x10"]
     tileArray = np.ndarray((1, 1), dtype = np.object)
     
@@ -23,7 +24,7 @@ class GM:
         self.timeRunning = False
         self.bestTimeLabel.configure(text = "Best overall time: " + str(self.GetBestTime()))
 
-    #Get text thats displayed on the buttons
+    #Get text thats displayed on the game button
     def GetTileText(self, tile):
         if tile.bomb:
             return "*"
@@ -36,11 +37,11 @@ class GM:
 
     #Generates game button grid
     def GenerateButtonGrid(self):
-        #Remove previous widgets
+        #Remove previous grid
         for widget in self.gameFrame.winfo_children():
             widget.destroy()
 
-        #Make grid
+        #Make new button grid
         for y in range(self.tileArray.shape[0]):
             for x in range(self.tileArray.shape[1]):
                 newButton = tk.Button(self.gameFrame, height = 1, width = 2)
@@ -52,9 +53,9 @@ class GM:
     def UpdateButtonGrid(self, showAll = False):
         for y in range(self.tileArray.shape[0]):
             for x in range(self.tileArray.shape[1]):
-                if showAll:
+                if showAll: #If game over show all board
                     self.tileArray[y, x].button.configure(text = self.GetTileText(self.tileArray[y, x]), bg = "light gray")
-                else:
+                else: #If game is not over
                     if self.tileArray[y, x].revealed:
                         self.tileArray[y, x].button.configure(text = self.GetTileText(self.tileArray[y, x]), bg = "light gray")
                     elif self.tileArray[y, x].flag:
@@ -62,10 +63,11 @@ class GM:
                     else:
                         self.tileArray[y, x].button.configure(text = " ",  bg = "gray")
                 
-    #Checks if given coordinates are valid for array
+    #Checks if given coordinates are valid for tile array
     def ValidCoord(self, x, y):
         return x >= 0 and y >= 0 and x < self.tileArray.shape[1] and y < self.tileArray.shape[0]
 
+    #Checks if tile is a bomb
     def Isbomb(self, x, y):
         if(self.ValidCoord(x, y)):
             if(self.tileArray[y, x] == None):
@@ -77,8 +79,8 @@ class GM:
     #Generates game tiles array
     def GenerateTiles(self):
         #Generate random bombs
-        for a in range(int(self.mineSpinBox.get())):
-            while(True):
+        for a in range(int(self.mineSpinBox.get())): #Get how many bombs needed to place
+            while(True): #While loop to search for a new bomb tile if last one is already occupied
                 x = np.random.randint(0, self.tileArray.shape[1])
                 y = np.random.randint(0, self.tileArray.shape[0])
                 if(not self.Isbomb(y, x)): #Check if tile isnt already a bomb
@@ -96,7 +98,7 @@ class GM:
                                 neighbourBombs += 1
                     self.tileArray[y, x] = Tile(neighbourBombs)
                     
-
+    #Updates time in miliseconds
     def UpdateClock(self):
         self.time += 0.01
         timeString = "Time: " + "%.2f" % self.time
@@ -104,6 +106,7 @@ class GM:
         if(self.timeRunning):
             self.root.after(10, self.UpdateClock)
 
+    #Game start button pressed
     def StartButtonPressed(self, event):
         if len(self.sizeListBox.curselection()) == 1:  #Check if board size is selected
             self.flagsPlaced = 0
@@ -111,11 +114,13 @@ class GM:
             sizeX = int(size.split("x")[0])
             sizeY = int(size.split("x")[1])
 
+            #Generate tile grid and button board
             self.tileArray = np.ndarray((sizeY, sizeX), dtype=np.object)
             self.GenerateTiles()
             self.GenerateButtonGrid()
             self.UpdateButtonGrid()
 
+            #Initiate timer
             self.time = 0
             self.timeRunning = True
             self.UpdateClock()
@@ -130,7 +135,7 @@ class GM:
             highscore = str(100000000)
             f.write(highscore)
         except OSError as err:
-                    print("Error: ", err)
+            print("Error: ", err)
         finally:
             f.close()
             return highscore
@@ -140,7 +145,7 @@ class GM:
         if win:
             tk.messagebox.showinfo(title="Game over", message="You won. Time:  " + "%.2f" % self.time + " seconds")
             bestTime = self.GetBestTime()
-            if self.time < float(bestTime): #Compare best time to current time, replace if needed
+            if self.time < float(bestTime): #Compare best time to current time, replace if better
                 try:
                     f = open("highscore.txt", "w")
                     f.write(str( "%.2f" % self.time))
@@ -148,11 +153,13 @@ class GM:
                     self.bestTimeLabel.configure(text = "Best overall time: " + str(self.GetBestTime()))
                 except OSError as err:
                     print("Error: ", err)
-        else:
+        else: #lost
             tk.messagebox.showerror(title="Game over", message="You lost")
+        #Show all button grid
         self.GenerateButtonGrid()
         self.UpdateButtonGrid(True)
 
+    #Checks if player placed flags on all buttons
     def CheckWin(self):
         for y in self.tileArray:
             for x in y:
@@ -163,28 +170,28 @@ class GM:
     #Function called on button press
     def TileButtonPressed(self, x, y):
         if self.timeRunning:
-            if self.selecting.get(): #Selecting tile
+            if self.selecting.get(): #If selecting tile
                 if self.tileArray[y, x].bomb:
                     self.GameOver(False)
                 else:
                     self.tileArray[y, x].revealed = True
-                    if self.tileArray[y, x].number == 0:
+                    if self.tileArray[y, x].number == 0: #Presed an empty tile
                         self.RevealEmptyTilesAround(x, y)
                     self.UpdateButtonGrid() 
-            else: #Placing flag
-                if self.tileArray[y, x].flag:
+            else: #If placing flag
+                if self.tileArray[y, x].flag: #Deselecting flag
                     self.flagsPlaced -= 1
                     self.tileArray[y, x].flag = False
                     self.UpdateButtonGrid() 
 
-                elif self.flagsPlaced < int(self.mineSpinBox.get()):
+                elif self.flagsPlaced < int(self.mineSpinBox.get()): #Placing flag
                     self.tileArray[y, x].flag = True
                     self.flagsPlaced += 1
                     self.UpdateButtonGrid() 
                     if self.CheckWin():
                         self.GameOver(True)
         
-    #If pressed 0 then reveal all other connected zeroes
+    #If pressed empty tile show all other empty tiles around it
     def RevealEmptyTilesAround(self, x, y):
         for xx in range(-1, 2):
             for yy in range(-1, 2):
--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -2,47 +2,49 @@ import tkinter as tk
 import numpy as np
 import GameManager as gm
 
+#Make a tkinter window
 w = tk.Tk()
 w.title("Minesweeper")
 w.geometry("600x400")
 w.minsize("600", "400")
 
+#Frame for game configuration
 infoFrame = tk.Frame(w)
 infoFrame.pack(side="left", fill = "y")
 
-gameLabel = tk.Label(infoFrame,text = "Minesweeper", font=("Arial", 20))
-
-gameSizeLabel = tk.Label(infoFrame,text = "Board size")
-
-gameLabel.pack(side = "top")
-
-sizeListBox = tk.Listbox(infoFrame, height = 3)
+#Frame for game button grid
 gameFrame = tk.Frame(w)
 gameFrame.pack(fill = "both", expand = "yes")
-mineSpinBox = tk.Spinbox(infoFrame, from_ = 1, to = 25)
 
+gameLabel = tk.Label(infoFrame,text = "Minesweeper", font=("Arial", 20))
+gameLabel.pack(side = "top")
+
+gameSizeLabel = tk.Label(infoFrame,text = "Board size")
+gameSizeLabel.pack()
+
+#Create interactive widgets
+sizeListBox = tk.Listbox(infoFrame, height = 3)
+mineSpinBox = tk.Spinbox(infoFrame, from_ = 1, to = 25)
 timeLabel = tk.Label(infoFrame,text = "Time: 0.00")
 bestTimeLabel = tk.Label(infoFrame,text = "Best overall time: 0.00")
 
+#Create main game manager
 gameManager = gm.GM(w, gameFrame, sizeListBox, mineSpinBox, timeLabel, bestTimeLabel)
 
-gameSizeLabel.pack()
 #Add different board sizes to list box
 for x in range(len(gameManager.gameSizeList)):
     sizeListBox.insert(x, gameManager.gameSizeList[x])
 sizeListBox.pack()
 
+#Radio buttons for flag/selection selecting
 selectionRadioButton1 = tk.Radiobutton(infoFrame, text = "Selection", variable = gameManager.selecting, value = True)
 selectionRadioButton1.pack()
 selectionRadioButton2 = tk.Radiobutton(infoFrame, text = "Flag", variable = gameManager.selecting, value = False)
 selectionRadioButton2.pack()
 
-
-
 mineLabel = tk.Label(infoFrame,text = "Mines amount")
 mineLabel.pack()
 mineSpinBox.pack()
-
 
 bestTimeLabel.pack(side = "top")
 timeLabel.pack(side = "top")
@@ -50,6 +52,5 @@ timeLabel.pack(side = "top")
 startButton = tk.Button(infoFrame, text="Start", width = 10, height = 2)
 startButton.bind("<Button-1>", gameManager.StartButtonPressed)
 startButton.pack(side="bottom", fill = "x")
-
 
 w.mainloop()
